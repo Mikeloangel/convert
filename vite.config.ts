1066e3491f995c16269d0feb6a010a440f236793
Mikhail Varushichev <mikeloangel@gmail.com>
2025-06-13 11:21:42 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const repoName = 'convert';
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
 })
+
